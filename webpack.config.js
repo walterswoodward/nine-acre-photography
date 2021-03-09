7a35cea2c618ea7fdf387b459bbd49624a9879f2
walterswoodward <walter.sekope.woodward@gmail.com>
2021-03-09 10:54:29 -0800

    Reverting previous commit's changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const webpack = require('webpack');
 
 module.exports = {
     resolve: {
@@ -7,10 +6,4 @@ module.exports = {
             '@': path.resolve('resources/js'),
         },
     },
-    plugins: [
-        new webpack.DefinePlugin({
-          __VUE_OPTIONS_API__: false,
-          __VUE_PROD_DEVTOOLS__: false,
-        }),
-      ],
 };
