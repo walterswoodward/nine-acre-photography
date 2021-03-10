96c7711ce301c289c3a1381e1378a9c66d9dd727
walterswoodward <walter.sekope.woodward@gmail.com>
2021-03-09 17:27:13 -0800

    Replaces svg with minified version

--- public/js/app.js
+++ public/js/app.js
@@ -19661,7 +19661,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("/images/camera__w130.svg?c10f14a118607da65fd2e81b333f61ea");
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("/images/camera__w130.svg?27c867841ce6d17a053f8efaec630391");
 
 /***/ }),
 
