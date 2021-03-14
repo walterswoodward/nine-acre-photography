bf9f85e3c311c8bd2c3dc6652e7c6bc68396f525
walterswoodward <walter.sekope.woodward@gmail.com>
2021-03-13 19:30:15 -0800

    Continue to build out image lightbox - Adds fontawesome - Begins creating ui for for lightbox

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # nine-acre-photography
+## Getting Started
+* 
+## Development
+* `php artisan serve`
+* `npm run watch-poll`
--- resources/js/Components/LightboxImage.vue
+++ resources/js/Components/LightboxImage.vue
@@ -1,105 +1,195 @@
 <template>
-<div>
-  <div class="images flex justify-center flex-wrap" v-if="!showModal">
-    <div class="image" v-for="(image, index) in images" v-bind:key="index">
-      <img
-        class="m-1"
-        v-bind:src="'https://res.cloudinary.com/nineacrephotography/image/upload/c_scale,w_300' + image.path"
-        v-on:click="toggleModal"
-        v-bind:path="image.path"
-      />
+  <div>
+    <div class="images flex justify-center flex-wrap" v-if="!showModal">
+      <div class="image" v-for="(image, index) in images" v-bind:key="index">
+        <img
+          class="m-1"
+          v-bind:src="
+            'https://res.cloudinary.com/nineacrephotography/image/upload/c_scale,w_300' +
+            image.path
+          "
+          v-on:click="toggleModal"
+          v-bind:path="image.path"
+        />
+      </div>
     </div>
-  </div>
-  <div class="lightbox__wrap" v-bind:key="0" v-if="showModal" v-on:click="toggleModal">
-    <button class="modal__close" v-on:click="toggleModal">x</button>
-    <button class="arrow__right">&gt;</button>
-    <button class="arrow__left">&lt;</button>
-    <img
-      class="lightbox__image"
-      v-bind:src="'https://res.cloudinary.com/nineacrephotography/image/upload' + modalImagePath"
-      v-on:click="lightbox"
-    />
-  </div>
+    <div class="lightbox__wrap" v-bind:key="0" v-if="showModal">
+      <!-- <button class="arrow__right"><font-awesome-icon icon="angle-double-right" /></button> -->
+      <!-- <button class="arrow__left"><font-awesome-icon :icon="['fas', 'angle-double-left']" /></button> -->
+      <div
+        class="lightbox__image"
+        v-bind:style="{
+          backgroundImage:
+            'url(https://res.cloudinary.com/nineacrephotography/image/upload' +
+            modalImagePath +
+            ')',
+        }"
+        v-on:click="lightbox"
+      >
+        <div class="lightbox-image__actions">
+          <div class="lightbox-image-actions__count">1/3</div>
+          <div class="lightbox-image-actions__actions">
+            <font-awesome-icon icon="share-alt" />
+            <font-awesome-icon icon="clone" />
+            <font-awesome-icon icon="search-minus" />
+            <font-awesome-icon icon="search-plus" />
+            <font-awesome-icon icon="file-download" />
+            <button class="modal__close" v-on:click="toggleModal">
+              <font-awesome-icon :icon="['fas', 'times']" />
+            </button>
+          </div>
+        </div>
+        <div class="lightbox-image__caption">
+          <p class="lightbox-image-caption__title">Title</p>
+          <p class="lightbox-image-caption__specs">Specs</p>
+          <p class="lightbox-image-caption__description">Description</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .image {
-    cursor: pointer;
+  cursor: pointer;
 }
 
-.arrow__right, .arrow__left {
-    position: absolute;
-    font-size: 6rem;
+.arrow__right,
+.arrow__left {
+  position: absolute;
+  font-size: 2.5rem;
 }
 
 .arrow__right {
-    right: 7rem;
+  right: 60px;
 }
 
 .arrow__left {
-    left: 7rem;
+  left: 60px;
+}
+
+.arrow__right:hover,
+.arrow__left:hover {
+  color: #fff;
 }
 
 .modal__close {
-    position: absolute;
-    right: 3rem;
-    font-size: 3rem;
-    top: 1rem;
+  /* position: absolute;
+  right: 3rem;
+  font-size: 3rem;
+  top: 1rem; */
 }
 
 .lightbox__wrap {
-  background: rgba(0,0,0,0.5);
+  background: rgba(0, 0, 0, 0.5);
   position: absolute;
-  left:0;
+  left: 0;
   width: 100%;
   height: calc(100vh - 3.5rem);
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
 }
 
 .lightbox__image {
   display: flex;
-  width: 50%;
+  width: 100%;
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100%;
 }
+
+.lightbox-image__actions,
+.lightbox-image__caption {
+  position: fixed;
+  background: rgba(0, 0, 0, 0.8);
+  width: 100%;
+  color: #fff;
+  padding: 10px;
+  text-align: center;
+}
+
+.lightbox-image__caption {
+  bottom: 0;
+}
+
+.lightbox-image-caption__specs,
+.lightbox-image-caption__description {
+  font-size: 0.75rem;
+}
+
+.lightbox-image__actions {
+  display: flex;
+  justify-content: space-between;
+}
+
+.lightbox-image-actions__actions {
+    width: 30%;
+    display:flex;
+    min-width: 150px;
+    justify-content: space-around;
+    align-items: center;
+}
+
+
 </style>
 
 <script>
-var cl = cloudinary.Cloudinary.new({ cloud_name: "demo" });
-cl.responsive();
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faAngleDoubleLeft,
+  faAngleDoubleRight,
+  faTimes,
+  faShareAlt,
+  faClone,
+  faSearchMinus,
+  faSearchPlus,
+  faFileDownload,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(
+  faAngleDoubleLeft,
+  faAngleDoubleRight,
+  faTimes,
+  faShareAlt,
+  faClone,
+  faSearchMinus,
+  faSearchPlus,
+  faFileDownload
+);
+
 export default {
+  components: {
+    FontAwesomeIcon,
+  },
   props: {},
   methods: {
     toggleModal: function (event) {
       event.stopPropagation();
-      this.modalImagePath = event.target.getAttribute('path');
+      this.modalImagePath = event.target.getAttribute("path");
       this.showModal = !this.showModal;
     },
   },
   data: function () {
     return {
-    showModal: false,
-    modalImage: null,
+      showModal: false,
+      modalImage: null,
       images: [
         {
           name: "bicycle.jpg",
           path: "/v1604249965/samples/people/bicycle.jpg",
         },
         {
-             name: 'jazz.jpg',
-          path:
-            "/v1604249963/samples/people/jazz.jpg",
+          name: "jazz.jpg",
+          path: "/v1604249963/samples/people/jazz.jpg",
         },
         {
-             name: 'boy-snow-hoodie.jpg',
-          path:
-            "/v1604249963/samples/people/boy-snow-hoodie.jpg",
+          name: "boy-snow-hoodie.jpg",
+          path: "/v1604249963/samples/people/boy-snow-hoodie.jpg",
         },
         {
-             name: 'smiling-man.jpg',
-          path:
-            "/v1604249961/samples/people/smiling-man.jpg",
+          name: "smiling-man.jpg",
+          path: "/v1604249961/samples/people/smiling-man.jpg",
         },
       ],
     };
